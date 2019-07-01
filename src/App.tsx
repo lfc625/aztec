779b48293f57637ad165028f12bdda71e17e25b6
lfc <lfc625@qq.com>
2019-07-01 15:34:49 +0800

    标题

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>这是一个react应用</p>
         <a
           className="App-link"
           href="https://reactjs.org"
