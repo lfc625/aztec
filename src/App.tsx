052da6565e0b57eff149a096e8cddbea6bf8a080
aaa Alex <>
2019-07-01 15:31:12 +0800

    删除标题

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit ashdkashdkjhask <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
