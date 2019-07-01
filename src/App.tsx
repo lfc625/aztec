4bf945f1a7b9d3775c6eb072a019ec6f4f3f6501
alex <lfc625@foxmail.com>
2019-07-01 16:05:12 +0800

    过滤idea

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>这是一个react应用</p><a href={'https://www.baidu.com'}></a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit ashdkashdkjhask <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
