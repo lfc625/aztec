4c9c78967d01aa7d1a4b997bab5e1df4b8ff47fc
alex <lfc625@foxmail.com>
2019-07-08 17:18:37 +0800

    暂提交

---
+++ src/component/Header/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Button } from 'antd'
+
+class Header extends React.Component {
+    state = {
+        count: 5
+    }
+    render() {
+        return (
+            <Button type="primary">123</Button>
+        )
+    }
+}
+export default Header
---
+++ src/component/SlideBar/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { Button } from 'antd'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <Button>123</Button>
+        )
+    }
+}
+export default Header
